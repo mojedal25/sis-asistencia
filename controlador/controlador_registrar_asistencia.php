2e45c15d2832127fd5c862f2c12af88d33e64c0d
mojedal25 <manuelojedaleon@gmail.com>
2022-12-27 20:32:16 -0500

    Se agregaron los cambios para registrar la salida del empleado

--- controlador/controlador_registrar_asistencia.php
+++ controlador/controlador_registrar_asistencia.php
@@ -69,3 +69,79 @@ if (!empty($_POST["btnentrada"])) {
 <?php }
 
 ?>
+
+<!-- Registro de Salida  -->
+
+<?php
+
+if (!empty($_POST["btnsalida"])) {
+    if (!empty($_POST["txtdni"])) { 
+        $dni=$_POST["txtdni"];
+        $consulta=$conexion->query(" select count(*) as 'total' from empleado where dni='$dni' ");
+        $id=$conexion->query(" select id_empleado from empleado where dni='$dni' ");
+        if ($consulta->fetch_object()->total > 0) {
+
+            $fecha=date("Y-m-d h:i:s");
+            $id_empleado=$id->fetch_object()->id_empleado;
+            $busqueda=$conexion->query(" select id_asistencia from asistencia where id_empleado=$id_empleado order by id_asistencia desc limit 1 ");
+            $id_asistencia=$busqueda->fetch_object()->id_asistencia;
+            $sql=$conexion->query(" update asistencia set salida='$fecha' where id_asistencia=$id_asistencia ");
+            if ($sql == true) { ?>
+                <script>
+                    $(function notificacion(){
+                        new PNotify({
+                            title:"CORRECTO",
+                            type:"success",
+                            text:"ADIOS, VUELVE PRONTO",
+                            styling:"bootstrap3"
+                        })
+                    })
+                </script>
+            <?php } else { ?>
+                <script>
+                    $(function notificacion(){
+                        new PNotify({
+                            title:"INCORRECTO",
+                            type:"error",
+                            text:"Error al registrar SALIDA",
+                            styling:"bootstrap3"
+                        })
+                    })
+                </script>
+            <?php }
+            
+
+            
+        } else { ?>
+            <script>
+                $(function notificacion(){
+                    new PNotify({
+                        title:"INCORRECTO",
+                        type:"error",
+                        text:"El DNI ingreaso no existe",
+                        styling:"bootstrap3"
+                    })
+                })
+            </script>
+       <?php }
+            
+    } else { ?>
+        <script>
+                $(function notificacion(){
+                    new PNotify({
+                        title:"INCORRECTO",
+                        type:"error",
+                        text:"Ingrese el DNI",
+                        styling:"bootstrap3"
+                    })
+                })
+            </script>
+    <?php } ?>
+        <script>
+            setTimeout(() => {
+                window.history.replaceState(null,null,window.location.pathname);
+            }, 0);
+        </script>
+<?php }
+
+?>
